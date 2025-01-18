741d93b02dbca19bfda0a7974671cc56c0de5d07
buttovvski <yaroslavbut1@gmail.com>
2025-01-18 14:10:48 +0200

    Hotfix: FuncA calculates first 3 terms

--- FuncClass.cpp
+++ FuncClass.cpp
@@ -1,6 +1,12 @@
 #include "FuncClass.h"
 
 // Placeholder implementation, returns 0
-double FuncClass::FuncA() {
-    return 0.0;
+double FuncClass::FuncA(double x) {
+    double result = 0.0;
+    for (int i = 0; i < 3; ++i) {
+        double term = std::pow(x, i) / std::tgamma(i + 1);
+        result += term;
+    }
+    return result;
 }
+

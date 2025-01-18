4149fc265ecc814538c33b815af92619d3a6161f
buttovvski <yaroslavbut1@gmail.com>
2025-01-18 14:08:53 +0200

    initial commit

---
+++ FuncClass.cpp
@@ -0,0 +1,6 @@
+#include "FuncClass.h"
+
+// Placeholder implementation, returns 0
+double FuncClass::FuncA() {
+    return 0.0;
+}
---
+++ FuncClass.h
@@ -0,0 +1,10 @@
+#ifndef FUNC_CLASS_H
+#define FUNC_CLASS_H
+
+class FuncClass {
+public:
+    // Function to calculate a trigonometric function, placeholder return 0
+    double FuncA();
+};
+
+#endif // FUNC_CLASS_H
